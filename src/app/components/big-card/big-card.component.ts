a3440d6c6742e6b144978fccd54172eeccda2b9b
geisabitt <ageisislane@gmail.com>
2023-12-31 16:49:42 -0300

    feat: added page title menu

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.scss'],
+})
+export class BigCardComponent {}
