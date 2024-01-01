94a13552a8556d017ea38952fc1654c5f657f381
geisabitt <ageisislane@gmail.com>
2024-01-01 15:39:46 -0300

    refactor: making big card dynamic

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.scss'],
 })
-export class BigCardComponent {}
+export class BigCardComponent {
+  @Input()
+  photoCover: string = '';
+  @Input()
+  bigTitle: string = '';
+  @Input()
+  bigDescription: string = '';
+}
