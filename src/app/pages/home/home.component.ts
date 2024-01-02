72d0cdba0919c3ccdf736515bce4a2e7128ad599
geisabitt <ageisislane@gmail.com>
2024-01-01 23:37:01 -0300

    feat: added pagination to the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,11 +9,26 @@ import { Article } from '../../components/types/article';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  artigos: Article[] = dataFake;
+  articles: Article[] = dataFake;
   displaySmallCard: number = 4;
   displayBigCard: number = 1;
+  startBig: number = 0;
+  startSmall: number = 1;
 
-  constructor() {
-    console.log(this.artigos.length)
+  nextPage() {
+    if (this.articles.length > this.displaySmallCard) {
+      this.displayBigCard += 4;
+      this.displaySmallCard += 4;
+      this.startBig += 4;
+      this.startSmall += 4;
+    }
+  }
+  prevPage() {
+    if (this.displayBigCard !== 1) {
+      this.displayBigCard -= 4;
+      this.displaySmallCard -= 4;
+      this.startBig -= 4;
+      this.startSmall -= 4;
+    }
   }
 }
